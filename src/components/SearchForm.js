5bdb75b0b91126f75c502624612861818b254b9c
AbdiweliAli <ilweyn@hotmail.com>
2023-02-02 09:18:24 +0000

    new commit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,19 +9,39 @@ import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 
 import FormControl from "@mui/material/FormControl";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-export const SearchForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("submit");
+export const SearchForm = ({ setSearchTerm }) => {
+  const initialValues = {
+    searchTerm: "",
   };
+
+  const validationSchema = Yup.object({
+    searchTerm: Yup.string().required("Please enter a valid search term."),
+  });
+
+  const onSubmit = (formData) => {
+    setSearchTerm(formData.searchTerm);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
   return (
-    <Box sx={{ m: 1 }} component="form" onSubmit={handleSubmit}>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
-        <InputLabel htmlFor="searchTerm">search Term</InputLabel>
+    <Box sx={{ m: 1 }} component="form" onSubmit={formik.handleSubmit}>
+      <FormControl sx={{ m: 1 }} fullWidth variant="standard">
+        <InputLabel htmlFor="searchTerm">Search term</InputLabel>
         <Input
           id="searchTerm"
+          name="searchTerm"
           type="text"
+          value={formik.values.searchTerm}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           endAdornment={
             <InputAdornment position="end">
               <IconButton type="submit">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,22 @@ import Container from "@mui/material/Container";
 import { Stack } from "@mui/system";
 import { SearchForm } from "../components/SearchForm";
 import { Counteries } from "../components/Counteries";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [searchTerm, setSearchTerm] = useState();
+
+  useEffect(() => {
+    if (searchTerm) {
+      console.log("API CALL");
+    }
+  }, [searchTerm]);
+
   return (
     <Container maxWidth="xl">
       <Stack gap={3}>
-        <SearchForm />
+        <SearchForm setSearchTerm={setSearchTerm} />
+
         <Counteries />
       </Stack>
     </Container>
